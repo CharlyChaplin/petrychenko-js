7b97ee0612b4faa2a6ea2b35a23f04919c67ed64
CharyChaplin <charlychaplin83@gmail.com>
2023-02-12 17:41:32 +0700

    033

--- js/script.js
+++ js/script.js
@@ -1,83 +1,89 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
 
-const movieDB = {
-	movies: [
-		"Логан",
-		"Лига справедливости",
-		"Ла-ла лэнд",
-		"Одержимость",
-		"Скотт Пилигрим против..."
-	]
-};
+document.addEventListener("DOMContentLoaded", function () {
+	const movieDB = {
+		movies: [
+			"Логан",
+			"Лига справедливости",
+			"Ла-ла лэнд",
+			"Одержимость",
+			"Скотт Пилигрим против..."
+		]
+	};
 
-document.querySelector(".promo__adv").remove();
-document.querySelector('.promo__content').style.width = "100%";
-document.querySelector(".promo__genre").textContent = "ДРАМА";
-document.querySelector(".promo__bg").style.backgroundImage = "url('../img/bg.jpg')";
+	document.querySelector(".promo__adv").remove();
+	document.querySelector('.promo__content').style.width = "100%";
+	document.querySelector(".promo__genre").textContent = "ДРАМА";
+	document.querySelector(".promo__bg").style.backgroundImage = "url('../img/bg.jpg')";
 
-//const input = document.querySelector(".adding__input");
-//input.addEventListener("keypress", handleKey);
-//const sendBtn = document.querySelector(".add button");
-//sendBtn.addEventListener("click", handleClick);
-const list = document.querySelector(".promo__interactive-list");
+	const input = document.querySelector(".adding__input");
+	input.addEventListener("keypress", handleKey);
+	const sendBtn = document.querySelector(".add button");
+	sendBtn.addEventListener("click", handleClick);
+	const form = document.querySelector(".add");
+	const chk = form.querySelector("[type='checkbox']");
+	const span_chk = form.querySelector(".yes");
+	const list = document.querySelector(".promo__interactive-list");
 
-render();
+	span_chk.addEventListener("click", handleSpanChk);
 
-//function deleteListItem(i) {
-//	list.children[i].remove();
-//	movieDB.movies = movieDB.movies.filter((_, index) => index !== i);
-//	render();
-//};
-//function handleKey(e) {
-//	if (e.key === "Enter") {
-//		if (e.target.value.toString().length > 0) {
-//			e.preventDefault();
-//			addItem(String(input.value));
-//			e.target.value = "";
-//		} else {
-//			e.preventDefault();
-//			console.log("Empty input");
-//		}
-//	}
-//};
-//function handleClick(e) {
-//	e.preventDefault();
-//	if (input.value.toString().length > 0) {
-//		addItem(String(input.value));
-//		input.value = "";
-//	} else {
-//		console.log("Empty input");
-//	}
-//};
-//function addItem(text) {
-//	movieDB.movies = [...movieDB.movies, text];
-//	render();
-//};
+	function handleSpanChk() { chk.checked = !chk.checked };
 
-function render() {
-	list.innerHTML = '';
-	movieDB.movies.sort().forEach((item, index) => {
-		const listItem = document.createElement("li");
-		listItem.classList.add("promo__interactive-item");
-		listItem.textContent = `${index + 1}.) ${item}`;
-		const delBtn = document.createElement("div");
-		delBtn.classList.add("delete");
-		//delBtn.addEventListener("click", () => deleteListItem(index));
-		listItem.appendChild(delBtn);
-		list.appendChild(listItem);
-	});
-};
+	render();
+
+	function deleteListItem(i) {
+		list.children[i].removeEventListener("click", deleteListItem);
+		movieDB.movies = movieDB.movies.filter((_, index) => index !== i);
+		render();
+	};
+	function handleKey(e) {
+		if (e.key === "Enter") {
+			e.preventDefault();
+			if (e.target.value.toString().length) addItem(String(input.value));
+		}
+	};
+	function handleClick(e) {
+		e.preventDefault();
+		if (input.value.toString().length) addItem(String(input.value));
+	};
+	function addItem(text) {
+		movieDB.movies = [...movieDB.movies, text];
+		chk.checked ? console.log("Adding favorite film") : '';
+		form.reset();
+		render();
+	};
+	function getCaption(txt) {
+		return txt.length < 21 ? txt : txt.substr(0, 21).concat("...");
+	}
+	function render() {
+		list.innerHTML = '';
+		movieDB.movies.sort().forEach((item, index) => {
+			const listItem = document.createElement("li");
+			listItem.classList.add("promo__interactive-item");
+			listItem.textContent = `${index + 1}.) ${getCaption(item)}`;
+			const delBtn = document.createElement("div");
+			delBtn.classList.add("delete");
+			delBtn.addEventListener("click", () => deleteListItem(index));
+			listItem.appendChild(delBtn);
+			list.appendChild(listItem);
+		});
+	};
+})
+
